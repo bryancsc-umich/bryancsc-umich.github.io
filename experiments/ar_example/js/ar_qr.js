7d9aac78da215dbceaca533dc7c3b68bf1f20aab
bryancsc <bryancsc@umich.edu>
2021-02-20 16:31:39 -0500

    Revert ar_qr.js back to simple version (not showing QR for card textures).

--- experiments/ar_example/js/ar_qr.js
+++ experiments/ar_example/js/ar_qr.js
@@ -16,47 +16,4 @@ window.addEventListener('load', function() {
     }
 
     alert ("Open " + scanner_url + " on your mobile phone and scan the QR below.");
-    new QRCode(this.document.getElementById('scanner_qr'), scanner_url);
-
-    let current_side = 'ar_qr';
-
-    function setScanHintText() {
-        if (current_side === 'ar_qr') {
-            document.getElementById('scan_hint').innerText = "Tap to view URL and QR code for AR-scanner.";
-        } else {
-            document.getElementById('scan_hint').innerText = scanner_url;
-        }
-    }
-
-    function setUpScanHint() {
-        let scan_timeout = null;
-
-        function showScanHint() {
-            if (scan_timeout !== null) {
-                clearTimeout(scan_timeout);
-            }
-            setScanHintText();
-            document.getElementById('scan_hint').style.visibility = 'visible';
-            scan_timeout = setTimeout(function() {
-                document.getElementById('scan_hint').style.visibility = 'hidden';
-                clearTimeout(scan_timeout);
-            }, 3000);    
-        }
-
-        this.document.getElementById('scan_hint').addEventListener('click', showScanHint);
-        showScanHint();
-    }
-
-    this.console.log(document.getElementById('qr_box'));
-    this.document.getElementById('qr_box').addEventListener('click', function() {
-        document.getElementById('qr_box_inner').classList.toggle('open');
-        if (current_side === 'ar_qr') {
-            current_side = 'scan_qr';
-        } else {
-            current_side = 'ar_qr';
-        }
-        setScanHintText();
-    });
-
-    setUpScanHint();
 });
